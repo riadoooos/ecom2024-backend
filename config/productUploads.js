68da93f3a63ea63c49c7d201b0d75eb0f8083c68
riadoooos <lotfi.said1955@gmail.com>
2024-03-11 11:41:54 +0100

    deployment

---
+++ config/productUploads.js
@@ -0,0 +1,30 @@
+import cloudinaryPackage from "cloudinary";
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+
+//configure cloudinary
+const cloudinary = cloudinaryPackage.v2;
+cloudinary.config({
+  /*cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,*/
+  cloud_name: "dtrz3i2f5",
+  api_key: "971769924893331",
+  api_secret: "K8KIpSAK5v8vQeyexi6mE9C244I",
+});
+
+// Create storage engine for Multer
+const storage = new CloudinaryStorage({
+  cloudinary,
+  allowedFormats: ["jpg", "png"],
+  params: {
+    folder: "ecommerce-novotek-2024-products",
+  },
+});
+
+// Init Multer with the storage engine
+const uploadProducts = multer({
+  storage,
+});
+
+export default uploadProducts;
